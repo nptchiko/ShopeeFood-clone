b6b3315a2e13c21fb8c7d4cbfe467b7e84a7b792
miku <letthewindtellyou@gmail.com>
2026-06-24 14:22:43 +0700

    init app

---
+++ src/main/java/org/intern/shopeefoodclone/ShopeeFoodCloneApplication.java
@@ -0,0 +1,13 @@
+package org.intern.shopeefoodclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeFoodCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopeeFoodCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/intern/shopeefoodclone/ShopeeFoodCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.intern.shopeefoodclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeFoodCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
