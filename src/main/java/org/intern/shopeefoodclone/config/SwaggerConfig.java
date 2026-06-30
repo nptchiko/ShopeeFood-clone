547ad2004484f1b3e4da763ec4d32eca315ab183
miku <letthewindtellyou@gmail.com>
2026-06-30 15:39:01 +0700

    feat: add swagger api docs

---
+++ src/main/java/org/intern/shopeefoodclone/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package org.intern.shopeefoodclone.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Shopee Food Clone API")
+                        .description("API documentation for Shopee Food Clone")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Developer")
+                                .email("developer@example.com"))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
