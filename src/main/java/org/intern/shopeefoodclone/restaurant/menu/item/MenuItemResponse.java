305519a3fca78fd6baf14267b78232970a921c4f
miku <letthewindtellyou@gmail.com>
2026-07-22 02:34:21 +0700

    perf: add JsonInclude & Builder to dto response

--- src/main/java/org/intern/shopeefoodclone/restaurant/menu/item/MenuItemResponse.java
+++ src/main/java/org/intern/shopeefoodclone/restaurant/menu/item/MenuItemResponse.java
@@ -1,8 +1,13 @@
 package org.intern.shopeefoodclone.restaurant.menu.item;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record MenuItemResponse(
         UUID id,
         UUID categoryId,
